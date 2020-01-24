cc7a80a3bcc6929712a67d414ef37c4302a14d1a
Shikeless <Shikeless@gmail.com>
2020-01-24 23:51:44 +0300

    refactoring, fixind routes

--- src/components/RootRoute/RootRoute.js
+++ src/components/RootRoute/RootRoute.js
@@ -1,24 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import "./RootRoute.css";
 import DataVolumeSelector from "../DataVolumeSelector";
-import SmallData from "../SmallData";
+import DataTable from "../DataTable";
 import Stub from "../Stub";
-import BigData from "../BigData";
-import { useStyles } from "./styles.js";
-import { Box, Paper, Typography, Grid, Button } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 
 function Root() {
-  const classes = useStyles();
-
   return (
     <Box p={2}>
       <BrowserRouter>
         <DataVolumeSelector />
         <Box>
           <Switch>
-            <Route exact path="/SmallData" component={SmallData} />
-            <Route exact path="/BigData" component={BigData} />
+            <Route path="/Data/:page" component={DataTable} />
             <Route exact path="/" component={Stub} />
             <Redirect to="/" />
           </Switch>
