835db2aa940417363390b9f8f96bf41a1c3f5586
Shikeless <Shikeless@gmail.com>
2020-01-24 23:56:31 +0300

    refactoring, adding new sagas

--- src/modules/Loader/sagas.js
+++ src/modules/Loader/sagas.js
@@ -1,38 +1,85 @@
-import { takeLatest, put, fork, call } from "redux-saga/effects";
+import { takeLatest, put, fork, call, select } from "redux-saga/effects";
 import {
-  smallDataRequest,
-  smallDataFailure,
-  smallDataSuccess,
-  bigDataRequest,
-  bigDataFailure,
-  bigDataSuccess
+  dataRequest,
+  dataFailure,
+  dataSuccess,
+  searchingRequest,
+  searchingFailure,
+  searchingSuccess,
+  sortingRequest,
+  sortingSuccess,
+  isLoadingRemove,
+  restoreDataRequest,
+  restoreDataSuccess,
+  addNewDataRequest,
+  addNewDataSuccess
 } from "./actions";
+import { getData } from "./loader";
+import { searchResult } from "../../helpers/search";
+import { sortResult } from "../../helpers/sort";
+import { save, load } from "../../helpers/localStorage";
 
 import { fetchSmallData, fetchBigData } from "./api.js";
 
-function* fetchCountWatcher(action) {
-  yield takeLatest(smallDataRequest, smallDataRequestFlow);
-  yield takeLatest(bigDataRequest, bigDataRequestFlow);
+function* fetchLoaderWatcher(action) {
+  yield takeLatest(dataRequest, dataRequestFlow);
+  yield takeLatest(searchingRequest, searchingRequestFlow);
+  yield takeLatest(sortingRequest, sortingRequestFlow);
+  yield takeLatest(restoreDataRequest, restoreDataRequestFlow);
+  yield takeLatest(addNewDataRequest, addNewDataRequestFlow);
 }
 
-function* smallDataRequestFlow() {
+function* dataRequestFlow(action) {
   try {
-    const data = yield call(fetchSmallData);
-    yield put(smallDataSuccess(data));
+    let data;
+    if (action.payload === "small") {
+      data = yield call(fetchSmallData);
+    } else {
+      data = yield call(fetchBigData);
+    }
+    yield call(save, "data", data);
+    yield put(dataSuccess(data));
+    yield put(isLoadingRemove());
   } catch (error) {
-    yield put(smallDataFailure());
+    yield put(dataFailure("Ошибка загрузки, повторите попытку"));
   }
 }
 
-function* bigDataRequestFlow() {
-  try {
-    const data = yield call(fetchBigData);
-    yield put(bigDataSuccess(data));
-  } catch (error) {
-    yield put(bigDataFailure());
-  }
+function* searchingRequestFlow(action) {
+  const data = yield select(getData);
+  const result = yield call(searchResult, data, action.payload);
+  result.length === 0
+    ? yield put(searchingFailure("Результаты поиска остутствуют"))
+    : yield put(searchingSuccess(result));
+  yield put(isLoadingRemove());
+}
+
+function* sortingRequestFlow(action) {
+  const data = yield select(getData);
+  const result = yield call(
+    sortResult,
+    data,
+    action.payload.direction,
+    action.payload.column
+  );
+  yield put(sortingSuccess(result));
+}
+
+function* restoreDataRequestFlow() {
+  const data = load("data");
+  yield put(isLoadingRemove());
+  yield put(restoreDataSuccess(data));
+}
+
+function* addNewDataRequestFlow(action) {
+  let data = yield call(load, "data");
+  yield put(restoreDataSuccess(data));
+  yield put(addNewDataSuccess(action.payload));
+  data = yield select(getData);
+  yield call(save, "data", data);
+  yield put(isLoadingRemove());
 }
 
 export default function* root() {
-  yield fork(fetchCountWatcher);
+  yield fork(fetchLoaderWatcher);
 }
